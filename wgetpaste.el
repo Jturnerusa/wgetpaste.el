77452a0732b3eaf05595d1dbba9167d0c9db79eb
John Turner <jturner.usa@gmail.com>
2022-11-16 00:45:35 -0500

    created wgetpaste customization group

--- wgetpaste.el
+++ wgetpaste.el
@@ -1,1 +1,11 @@
+(defgroup wgetpaste nil
+  "Wgetpaste Emacs functions")
+
+(defcustom wgetpaste-executable
+  "wgetpaste"
+  "Path to wgetpaste executable")
+
+(defcustom wgetpaste-buffer "*wgetpaste*"
+  "Buffer name used for wgetpaste output")
+
 (provide 'wgetpaste)
